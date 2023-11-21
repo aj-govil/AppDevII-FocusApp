eda076596a7f849a54114e8c20eca9b1d1085103
Zoidt <zaidtabana@gmail.com>
2023-11-21 15:40:41 -0500

    Update stats screen
    Change display text to ease with differentiating between different screens

--- app/src/main/java/com/example/ajeshgovil_assignment2/StatsScreen.kt
+++ app/src/main/java/com/example/ajeshgovil_assignment2/StatsScreen.kt
@@ -11,7 +11,7 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun StatsScreen(): Unit {
     Text(
-        text = "MY FAVORITE SONGS",
+        text = "MY FAVORITE STATISTICS SCREEN!!!!!",
         style = MaterialTheme.typography.headlineMedium,
         color = Color.Blue,
         modifier = Modifier.padding(8.dp)
