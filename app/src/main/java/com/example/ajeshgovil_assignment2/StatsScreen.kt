65df4abebdece9c1847dd4e28381c001e98353ce
Zoidt <zaidtabana@gmail.com>
2023-11-21 15:28:09 -0500

    Add temp screen
    Add a screen to serve as confirmation for navigation

---
+++ app/src/main/java/com/example/ajeshgovil_assignment2/StatsScreen.kt
@@ -0,0 +1,19 @@
+package com.example.ajeshgovil_assignment2
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun StatsScreen(): Unit {
+    Text(
+        text = "MY FAVORITE SONGS",
+        style = MaterialTheme.typography.headlineMedium,
+        color = Color.Blue,
+        modifier = Modifier.padding(8.dp)
+    )
+}
