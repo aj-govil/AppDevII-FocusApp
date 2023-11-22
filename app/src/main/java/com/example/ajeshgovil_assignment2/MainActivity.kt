e3f9914aaebc333959565ae5f1e40a03870749eb
Zoidt <zaidtabana@gmail.com>
2023-11-21 19:48:54 -0500

    Setup one screen navigation

--- app/src/main/java/com/example/ajeshgovil_assignment2/MainActivity.kt
+++ app/src/main/java/com/example/ajeshgovil_assignment2/MainActivity.kt
@@ -2,9 +2,11 @@
 
 package com.example.ajeshgovil_assignment2
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -22,16 +24,24 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.ajeshgovil_assignment2.ui.theme.AjeshGovil_Assignment2Theme
 
@@ -59,16 +69,37 @@ class MainActivity : ComponentActivity() {
  * Navigation inspired by the Navigation codelab
  * https://developer.android.com/codelabs/jetpack-compose-navigation
  */
+//@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun FocusApp() {
+    var currentScreen: FocusDestination by remember { mutableStateOf(Tasks) }
     val navController = rememberNavController()
 
+    Scaffold (
+        bottomBar = {
+
+        }
+    )
+    { innerPadding ->
+        // Navigation controlled through here
+        NavHost(
+            navController = navController,
+            startDestination = Tasks.route,
+            Modifier.padding(innerPadding)
+        ) {
+            // See destinations for which screen is passed in
+            composable(route = Tasks.route) {
+                Tasks.screen()
+            }
+        }
+    }
 }
 /**
  * Contains a favorite song input form along with a list of songs the user has entered.
  */
 @Composable
 fun SongListApp(viewModel: SongListViewModel) {
+
     Column(
         modifier = Modifier
             .fillMaxSize()
