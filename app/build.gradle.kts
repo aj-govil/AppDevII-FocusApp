559fe0b35d9ad855c06b96e6fae0e75b5587c8b1
ZaidT <zaidtabana@gmail.com>
2023-11-21 21:26:35 -0500

    directly inject version
    Instead of using variable, put version directly into dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,7 @@ android {
 dependencies {
     val nav_version = "2.7.5"
 
-    implementation ("androidx.navigation:navigation-compose:$nav_version")
+    implementation ("androidx.navigation:navigation-compose:2.7.5")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.0")
