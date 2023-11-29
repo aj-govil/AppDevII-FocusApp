3ad861078f0b2e6aa3bd7cc720a2bee206460f63
ZaidT <zaidtabana@gmail.com>
2023-11-29 16:58:36 -0500

    Remove viewmodel from task class

--- app/src/main/java/com/example/FocusApp/data/Task.kt
+++ app/src/main/java/com/example/FocusApp/data/Task.kt
@@ -4,9 +4,6 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.lifecycle.ViewModel
-import java.sql.Time
-import java.util.Calendar
-import java.util.Date
 
 
 data class Task(
@@ -15,20 +12,3 @@ data class Task(
     var dueTime: String,
     var isComplete: Boolean
 )
-
-class TaskListViewModel : ViewModel() {
-    // Data Fields for a Task
-    val title = mutableStateOf(TextFieldValue())
-    val description = mutableStateOf(TextFieldValue())
-    val dueTime = mutableStateOf(TextFieldValue())
-    val isComplete = mutableStateOf(false)
-    val taskList = mutableStateListOf<Task>()
-
-    // Data Fields for a Account
-    val firstName = mutableStateOf(TextFieldValue())
-    val lastName = mutableStateOf(TextFieldValue())
-    var age = mutableStateOf(TextFieldValue())
-    val email = mutableStateOf(TextFieldValue())
-
-
-}
