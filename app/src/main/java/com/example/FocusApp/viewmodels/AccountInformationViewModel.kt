56e62b38c529aee7191046ae25cbe20232875094
ZaidT <zaidtabana@gmail.com>
2023-11-29 16:59:39 -0500

    Create new view models
    AccountInformationViewModel has values commented out, will be undone once this view model is implemented

---
+++ app/src/main/java/com/example/FocusApp/viewmodels/AccountInformationViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.FocusApp.viewmodels
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.lifecycle.ViewModel
+
+// View Model for account information
+class AccountInformationViewModel: ViewModel() {
+
+    // Data Fields for a Account
+//    val firstName = mutableStateOf(TextFieldValue())
+//    val lastName = mutableStateOf(TextFieldValue())
+//    var age = mutableStateOf(TextFieldValue())
+//    val email = mutableStateOf(TextFieldValue())
+}
