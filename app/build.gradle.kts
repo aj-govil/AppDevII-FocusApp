7ac4afec8c5430691e4bf4a00174fc116d6e3067
Zoidt <zaidtabana@gmail.com>
2023-11-29 21:39:51 -0500

    Add firebase ui dependency
    per talib slides

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,6 +81,8 @@ dependencies {
 
     // Import the Firebase BoM
     implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
+    // FireBaseUI
+    implementation("com.firebaseui:firebase-ui-auth:7.2.0")
 
     // TODO: Add the dependencies for Firebase products you want to use
     // When using the BoM, don't specify versions in Firebase dependencies
