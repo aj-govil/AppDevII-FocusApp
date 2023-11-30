6d46e3a6a3e2b4dc786c77cc5b6137dac1a9e36b
Zoidt <zaidtabana@gmail.com>
2023-11-30 06:44:11 -0500

    Setup ProfileRepDatastore user profilerepository interface

--- app/src/main/java/com/example/FocusApp/model/ProfileRepositoryDataStore.kt
+++ app/src/main/java/com/example/FocusApp/model/ProfileRepositoryDataStore.kt
@@ -17,3 +17,34 @@ import androidx.datastore.preferences.preferencesDataStore
 //   and does not provide type safety.
 const val PROFILE_DATASTORE ="profile_datastore"
 private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = PROFILE_DATASTORE)
+
+class ProfileRepositoryDataStore (private val context: Context) : ProfileRepository  {
+    companion object {
+        val NAME = stringPreferencesKey("NAME")
+        val COUNTER = intPreferencesKey("COUNTER")
+    }
+
+    /** Update the values in the DataStore. */
+    override suspend fun saveProfile(profileData: ProfileData) {
+        context.dataStore.edit {
+            it[NAME] = profileData.name
+            it[COUNTER] = profileData.counter
+        }
+    }
+
+    /** Get the data in the DataStore as a flow.  Since the store may have never
+     *     been used yet, handle the null case with default values. */
+    override fun getProfile(): Flow<ProfileData> = context.dataStore.data.map {
+        ProfileData(
+            name = it[NAME] ?: "",
+            counter = it[COUNTER] ?: 0
+        )
+    }
+
+    override suspend fun clear() {
+        context.dataStore.edit {
+            it.clear()
+        }
+    }
+
+}
