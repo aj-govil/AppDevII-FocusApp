5ad411916b10b435b942b3bc3de62b918400b877
Zoidt <zaidtabana@gmail.com>
2023-11-29 22:16:45 -0500

    Setup AuthViewModel

--- app/src/main/java/com/example/FocusApp/auth/AuthViewModel.kt
+++ app/src/main/java/com/example/FocusApp/auth/AuthViewModel.kt
@@ -1,7 +1,37 @@
 package com.example.FocusApp.auth
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class AuthViewModel(private val authRepository: AuthRepository): ViewModel(){
 
+    // Return a StateFlow so that the composable can awlays update
+    // based when the value changes
+    fun currentUser(): StateFlow<User?>{
+        return authRepository.currentUser();
+    }
+
+    fun signUp(email: String, password: String){
+        viewModelScope.launch {
+            authRepository.signUp(email,password);
+        }
+    }
+
+    fun signIn(email: String, password: String){
+        viewModelScope.launch {
+            authRepository.signIn(email,password);
+        }
+    }
+
+    fun signOut(){
+        authRepository.signOut();
+    }
+
+    fun delete(){
+        viewModelScope.launch {
+            authRepository.delete();
+        }
+    }
 }
