dac7399303e7c00a73cb22c6fe80be263e408c49
Zoidt <zaidtabana@gmail.com>
2023-12-10 14:54:12 -0500

    setup result variables

--- app/src/main/java/com/example/FocusApp/auth/AuthViewModel.kt
+++ app/src/main/java/com/example/FocusApp/auth/AuthViewModel.kt
@@ -5,10 +5,24 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavController
 import com.example.FocusApp.MyApp
+import com.google.firebase.auth.FirebaseAuthException
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
 class AuthViewModel(private val authRepository: AuthRepository): ViewModel(){
+
+    private val _signInResult = MutableStateFlow<ResultAuth<Boolean>?>(ResultAuth.Inactive)
+    private val _signUpResult = MutableStateFlow<ResultAuth<Boolean>?>(ResultAuth.Inactive)
+    private val _signOutResult = MutableStateFlow<ResultAuth<Boolean>?>(ResultAuth.Inactive)
+    private val _deleteAccountResult = MutableStateFlow<ResultAuth<Boolean>?>(ResultAuth.Inactive)
+
+    val signInResult: StateFlow<ResultAuth<Boolean>?> = _signInResult
+    val signUpResult: StateFlow<ResultAuth<Boolean>?> = _signUpResult
+    val signOutResult: StateFlow<ResultAuth<Boolean>?> = _signOutResult
+    val deleteAccountResult: StateFlow<ResultAuth<Boolean>?> = _deleteAccountResult
 
     // Return a StateFlow so that the composable can awlays update
     // based when the value changes
