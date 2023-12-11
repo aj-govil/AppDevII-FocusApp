605ffdebf4d9ddc6e89dbe9226692c1e877d7438
Zoidt <zaidtabana@gmail.com>
2023-12-11 10:29:20 -0500

    init TaskRepsitory
    Interface for CRUD logic for Tasks

---
+++ app/src/main/java/com/example/FocusApp/db/TaskRepository.kt
@@ -0,0 +1,14 @@
+package com.example.FocusApp.db
+
+import com.example.FocusApp.data.Task
+import kotlinx.coroutines.flow.Flow
+
+interface TaskRepository {
+    suspend fun addTask(task: Task)
+    suspend fun getTasks(userId: String): Flow<List<Task>>
+
+    // suspend fun getTasks(): Flow<List<Task>> // Do we need to get all tasks?
+    suspend fun getTask(name: String): Flow<Task>
+
+    suspend fun delete(task: Task)
+}
