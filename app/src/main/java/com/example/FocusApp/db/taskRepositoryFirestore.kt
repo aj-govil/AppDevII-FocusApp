0d2a8a0d4c5e1731e405325ee3422334c9d663c1
Zoidt <zaidtabana@gmail.com>
2023-12-11 10:57:04 -0500

    implement addTask in taskRepository

--- app/src/main/java/com/example/FocusApp/db/taskRepositoryFirestore.kt
+++ app/src/main/java/com/example/FocusApp/db/taskRepositoryFirestore.kt
@@ -1,12 +1,23 @@
 package com.example.FocusApp.db
 
+import android.content.ContentValues.TAG
+import android.util.Log
 import com.example.FocusApp.data.Task
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.flow.Flow
 
 class taskRepositoryFirestore(val db: FirebaseFirestore): TaskRepository {
+
+    // get database
+    val dbTasks = db.collection("Tasks")
     override suspend fun addTask(task: Task) {
-        TODO("Not yet implemented")
+        dbTasks.add(task)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
     }
 
     override suspend fun getTasks(userId: String): Flow<List<Task>> {
